e05c08495d82a01676453f6fead73f44062e683f
teja00 <tejanagubandi00@gmail.com>
2024-06-17 21:43:03 -0500

    Color Added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 function App() {
   return (
     <Canvas>
+      <color attach="background" args={['lightblue']} />
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
