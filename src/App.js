8a03a59504a6d42d3a6dc8f0f72f27a30de7da4c
teja00 <tejanagubandi00@gmail.com>
2024-06-18 23:01:24 -0500

    basic Navbar

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Box from './components/Box';
-
+import NavbarHome from './components/Navbar';
 
 import './App.css';
 
 function App() {
   return (
-    <Canvas>
-      <color attach="background" args={['lightblue']} />
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls autoRotate={false} enablePan={false} minDistance={5} maxDistance={10} />
-    </Canvas>
+    // <Canvas>
+    //   <color attach="background" args={['lightblue']} />
+    //   <ambientLight intensity={Math.PI / 2} />
+    //   <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+    //   <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    //   <Box position={[-1.2, 0, 0]} />
+    //   <Box position={[1.2, 0, 0]} />
+    //   <OrbitControls autoRotate={false} enablePan={false} minDistance={5} maxDistance={10} />
+    // </Canvas>
+    <NavbarHome/>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 createRoot(document.getElementById('root')).render(<App />)
