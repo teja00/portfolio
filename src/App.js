ce01c90f954645559db6fc1c80ba01bd9bd913e1
teja00 <tejanagubandi00@gmail.com>
2024-06-19 19:40:13 -0500

    added the basic setting and template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-// import Skills from './components/Skills';
-// import Experience from './components/Experience';
-// import Education from './components/Education';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
+import React, { useState } from "react";
+import "./App.css";
+import Main from "./containers/Main";
+import { ThemeProvider } from "styled-components";
+import { themes } from "./theme";
+import { GlobalStyles } from "./global";
+import { CursorProvider } from "react-cursor-custom";
+import { settings } from "./portfolio";
+// import ReactGA from "react-ga";
 
 function App() {
+  // TODO we need to know how to add this later
+  // useEffect(() => {
+  //   if (settings.googleTrackingID) {
+  //     ReactGA.initialize(settings.googleTrackingID, {
+  //       testMode: process.env.NODE_ENV === "test",
+  //     });
+  //     ReactGA.pageview(window.location.pathname + window.location.search);
+  //   }
+  // }, []);
+
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+  const useCursor = settings.useCustomCursor;
+
   return (
-    <div className="App">
-      <Header />
-      <About />
-      {/* <Skills />
-      <Experience />
-      <Education />
-      <Projects />
-      <Contact />
-      <Footer /> */}
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <>
+        <GlobalStyles />
+        <div>
+          {useCursor ? (
+            <CursorProvider
+              color={themes[theme].secondaryText}
+              ringSize={25}
+              transitionTime={75}
+            >
+              <Main theme={themes[theme]} setTheme={setTheme} />
+            </CursorProvider>
+          ) : (
+            <Main theme={themes[theme]} setTheme={setTheme} />
+          )}
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
