48b902e5de5e501b2c587ccb4cec2da9979d5b33
teja00 <tejanagubandi00@gmail.com>
2024-06-19 19:26:00 -0500

    index.js changes made and pushed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BaseProvider, LightTheme } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+
 import App from './App';
 import './index.css';
+import "./assests/font-awesome/css/all.css";
+
+const engine = new Styletron();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </Router>
+  <StyletronProvider value={engine}>
+      <BaseProvider theme = {LightTheme}>
+          <App/>
+      </BaseProvider>
+  </StyletronProvider>
 );
