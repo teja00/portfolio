86aa0fb39e4021aaf25020f75b61ff9ce7d1bb4e
teja00 <tejanagubandi00@gmail.com>
2024-06-19 21:46:05 -0500

    few functionalities

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./theme";
 import { GlobalStyles } from "./global";
-import { CursorProvider } from "react-cursor-custom";
-import { settings } from "./portfolio";
+// import { CursorProvider } from "react-cursor-custom";
+// import { settings } from "./portfolio";
 // import ReactGA from "react-ga";
 
 function App() {
@@ -20,25 +20,13 @@ function App() {
   // }, []);
 
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
-  const useCursor = settings.useCustomCursor;
+  // const useCursor = settings.useCustomCursor;
 
   return (
     <ThemeProvider theme={themes[theme]}>
       <>
         <GlobalStyles />
-        <div>
-          {useCursor ? (
-            <CursorProvider
-              color={themes[theme].secondaryText}
-              ringSize={25}
-              transitionTime={75}
-            >
-              <Main theme={themes[theme]} setTheme={setTheme} />
-            </CursorProvider>
-          ) : (
             <Main theme={themes[theme]} setTheme={setTheme} />
-          )}
-        </div>
       </>
     </ThemeProvider>
   );
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,24 +1,105 @@
 import React from "react";
-// import { Route, Switch, HashRouter } from "react-router-dom";
-// import Home from "../pages/home/HomeComponent";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import Home from "../pages/home/HomeComponent";
 // import Splash from "../pages/splash/Splash";
 // import Education from "../pages/education/EducationComponent";
 // import Experience from "../pages/experience/Experience";
 // import Contact from "../pages/contact/ContactComponent";
 // import Projects from "../pages/projects/Projects";
-import { settings } from "../portfolio.js";
+// import { settings } from "../portfolio.js";
 
 export default function Main(propss) {
-  if (settings.isSplash) {
-    return (
+  // if (settings.isSplash) {
+  //   return (
+  //     <div>
+  //       <HashRouter basename="/">
+  //         <Routes>
+  //           <Route
+  //             path="/"
+  //             exact
+  //             render={(props) => (
+  //               <Splash
+  //                 {...props}
+  //                 theme={propss.theme}
+  //                 setTheme={propss.setTheme}
+  //               />
+  //             )}
+  //           />
+  //           <Route
+  //             path="/home"
+  //             render={(props) => (
+  //               <Home
+  //                 {...props}
+  //                 theme={propss.theme}
+  //                 setTheme={propss.setTheme}
+  //               />
+  //             )}
+  //           />
+  //           {/* <Route
+  //             path="/experience"
+  //             exact
+  //             render={(props) => (
+  //               <Experience
+  //                 {...props}
+  //                 theme={propss.theme}
+  //                 setTheme={propss.setTheme}
+  //               />
+  //             )}
+  //           />
+  //           <Route
+  //             path="/education"
+  //             render={(props) => (
+  //               <Education
+  //                 {...props}
+  //                 theme={propss.theme}
+  //                 setTheme={propss.setTheme}
+  //               />
+  //             )}
+  //           />
+  //           <Route
+  //             path="/contact"
+  //             render={(props) => (
+  //               <Contact
+  //                 {...props}
+  //                 theme={propss.theme}
+  //                 setTheme={propss.setTheme}
+  //               />
+  //             )}
+  //           />
+  //           <Route
+  //             path="/splash"
+  //             render={(props) => (
+  //               <Splash
+  //                 {...props}
+  //                 theme={propss.theme}
+  //                 setTheme={propss.setTheme}
+  //               />
+  //             )}
+  //           />
+  //           <Route
+  //             path="/projects"
+  //             render={(props) => (
+  //               <Projects
+  //                 {...props}
+  //                 theme={propss.theme}
+  //                 setTheme={propss.setTheme}
+  //               />
+  //             )}
+  //           /> */}
+  //         </Routes>
+  //       </HashRouter>
+  //     </div>
+  //   );
+  // } else {
+  return (
       <div>
-        {/* <HashRouter basename="/">
-          <Switch>
+        <HashRouter basename="/">
+          <Routes>
             <Route
               path="/"
               exact
               render={(props) => (
-                <Splash
+                <Home
                   {...props}
                   theme={propss.theme}
                   setTheme={propss.setTheme}
@@ -35,7 +116,7 @@ export default function Main(propss) {
                 />
               )}
             />
-            <Route
+            {/* <Route
               path="/experience"
               exact
               render={(props) => (
@@ -67,16 +148,6 @@ export default function Main(propss) {
               )}
             />
             <Route
-              path="/splash"
-              render={(props) => (
-                <Splash
-                  {...props}
-                  theme={propss.theme}
-                  setTheme={propss.setTheme}
-                />
-              )}
-            />
-            <Route
               path="/projects"
               render={(props) => (
                 <Projects
@@ -85,82 +156,10 @@ export default function Main(propss) {
                   setTheme={propss.setTheme}
                 />
               )}
-            />
-          </Switch>
+            /> */}
+          </Routes>
         </HashRouter>
       </div>
     );
-  } else {
-    return (
-      <div>
-        <HashRouter basename="/">
-          <Switch>
-            <Route
-              path="/"
-              exact
-              render={(props) => (
-                <Home
-                  {...props}
-                  theme={propss.theme}
-                  setTheme={propss.setTheme}
-                />
-              )}
-            />
-            <Route
-              path="/home"
-              render={(props) => (
-                <Home
-                  {...props}
-                  theme={propss.theme}
-                  setTheme={propss.setTheme}
-                />
-              )}
-            />
-            <Route
-              path="/experience"
-              exact
-              render={(props) => (
-                <Experience
-                  {...props}
-                  theme={propss.theme}
-                  setTheme={propss.setTheme}
-                />
-              )}
-            />
-            <Route
-              path="/education"
-              render={(props) => (
-                <Education
-                  {...props}
-                  theme={propss.theme}
-                  setTheme={propss.setTheme}
-                />
-              )}
-            />
-            <Route
-              path="/contact"
-              render={(props) => (
-                <Contact
-                  {...props}
-                  theme={propss.theme}
-                  setTheme={propss.setTheme}
-                />
-              )}
-            />
-            <Route
-              path="/projects"
-              render={(props) => (
-                <Projects
-                  {...props}
-                  theme={propss.theme}
-                  setTheme={propss.setTheme}
-                />
-              )}
-            />
-          </Switch>
-        </HashRouter> */}
-        nothing yet
-      </div>
-    );
   }
-}
+// }
