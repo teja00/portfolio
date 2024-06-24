3fa649b640c435d903e442cd5d15557f8a1aadc8
teja00 <tejanagubandi00@gmail.com>
2024-06-24 00:43:02 -0500

    added container to experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,6 +11,12 @@ import FallbackSpinner from './FallbackSpinner';
 import '../css/experience.css';
 
 const styles = {
+  Container: {
+    borderRadius: '15px',
+    padding: '40px',
+    margin: '0 auto',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  },
   ulStyle: {
     listStylePosition: 'outside',
     paddingLeft: 20,
@@ -50,55 +56,57 @@ function Experience(props) {
 
       {data
         ? (
-          <div className="section-content-container">
-            <Container>
-              <Timeline
-                lineColor={theme.timelineLineColor}
-              >
-                {data.map((item) => (
-                  <Fade>
-                    <TimelineItem
-                      key={item.title + item.dateText}
-                      dateText={item.dateText}
-                      dateInnerStyle={{ background: theme.accentColor }}
-                      style={styles.itemStyle}
-                      bodyContainerStyle={{ color: theme.color }}
-                    >
-                      <h2 className="item-title">
-                        {item.title}
-                      </h2>
-                      <div style={styles.subtitleContainerStyle}>
-                        <h4 style={{ ...styles.subtitleStyle, color: theme.accentColor }}>
-                          {item.subtitle}
-                        </h4>
-                        {item.workType && (
-                        <h5 style={styles.inlineChild}>
-                    &nbsp;·
-                          {' '}
-                          {item.workType}
-                        </h5>
-                        )}
-                      </div>
-                      <ul style={styles.ulStyle}>
-                        {item.workDescription.map((point) => (
-                          <div key={point}>
-                            <li>
-                              <ReactMarkdown
-                                children={point}
-                                components={{
-                                  p: 'span',
-                                }}
-                              />
-                            </li>
-                            <br />
-                          </div>
-                        ))}
-                      </ul>
-                    </TimelineItem>
-                  </Fade>
-                ))}
-              </Timeline>
-            </Container>
+          <div style={{ ...styles.Container, backgroundColor: theme.containerBackground }}>
+            <div className="section-content-container">
+              <Container>
+                <Timeline
+                  lineColor={theme.timelineLineColor}
+                >
+                  {data.map((item) => (
+                    <Fade>
+                      <TimelineItem
+                        key={item.title + item.dateText}
+                        dateText={item.dateText}
+                        dateInnerStyle={{ background: theme.accentColor }}
+                        style={styles.itemStyle}
+                        bodyContainerStyle={{ color: theme.color }}
+                      >
+                        <h2 className="item-title">
+                          {item.title}
+                        </h2>
+                        <div style={styles.subtitleContainerStyle}>
+                          <h4 style={{ ...styles.subtitleStyle, color: theme.accentColor }}>
+                            {item.subtitle}
+                          </h4>
+                          {item.workType && (
+                          <h5 style={styles.inlineChild}>
+                      &nbsp;·
+                            {' '}
+                            {item.workType}
+                          </h5>
+                          )}
+                        </div>
+                        <ul style={styles.ulStyle}>
+                          {item.workDescription.map((point) => (
+                            <div key={point}>
+                              <li>
+                                <ReactMarkdown
+                                  children={point}
+                                  components={{
+                                    p: 'span',
+                                  }}
+                                />
+                              </li>
+                              <br />
+                            </div>
+                          ))}
+                        </ul>
+                      </TimelineItem>
+                    </Fade>
+                  ))}
+                </Timeline>
+              </Container>
+            </div>
           </div>
         ) : <FallbackSpinner /> }
     </>
