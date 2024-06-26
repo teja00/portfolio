ae331d00c3e203673c005dd364d422e9b2458f38
teja00 <tejanagubandi00@gmail.com>
2024-06-26 02:34:41 -0500

    final deployment changes which seem to work

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -23,7 +23,7 @@ function MainApp() {
       <main className="main">
         <Switch>
           <Suspense fallback={<FallbackSpinner />}>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/portfolio" component={Home} />
             {data
               && data.sections.map((route) => {
                 const SectionComponent = React.lazy(() => import('./components/' + route.component));
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -60,7 +60,7 @@ const NavBar = () => {
       expanded={expanded}
     >
       <Container>
-        <Navbar.Brand href="/portfolio/home">Teja Nagubandi</Navbar.Brand>
+        <Navbar.Brand href="/portfolio">Teja Nagubandi</Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => setExpanded(!expanded)}
